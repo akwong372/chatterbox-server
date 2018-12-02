6e68b037dcd722e5d8c036ba57cce206de87b1a7
Sadie Gill <thesadie@gmail.com>
2018-12-01 17:22:40 -0800

    Adds client

---
+++ client/scripts/parse.js
@@ -0,0 +1,31 @@
+var Parse = {
+  //URL of API
+  server: 'http://127.0.0.1:3000/classes/messages',
+
+  create: function(message, successCB, errorCB = null) {
+    $.ajax({
+      url: Parse.server,
+      type: 'POST',
+      data: JSON.stringify(message), // take into account cross attacks (XSS)
+      contentType: 'application/json', //text
+      success: successCB,
+      error: errorCB || function(error) {
+        console.error('chatterbox: Failed to fetch messages', error);
+      }
+    });
+  },
+
+  readAll: function(successCB, errorCB = null) {
+    $.ajax({
+      url: Parse.server,
+      type: 'GET',
+      data: { order: '-createdAt' },
+      contentType: 'application/json',
+      success: successCB,
+      error: errorCB || function(error) {
+        console.error('chatterbox: Failed to fetch messages', error);
+      }
+    });
+  }
+
+};
---
+++ client/scripts/rooms.js
@@ -0,0 +1,11 @@
+var Rooms = {
+  // how to return current room name
+  // display only messages with roomname = to current room
+  add: function(roomname) {
+    var template = _.template(`<option value="<%- roomname %>"><%- roomname %></option>`);
+    $('#rooms select').append(template({roomname: roomname}));
+  }
+
+};
+
+  
